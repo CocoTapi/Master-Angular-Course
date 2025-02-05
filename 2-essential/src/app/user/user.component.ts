ab009fab9e165fe992a51f732ea4f930cac989ea
CocoTapi <shioriavino@gmail.com>
2025-02-05 16:25:00 -0500

    add task management features

--- 2-essential/src/app/user/user.component.ts
+++ 2-essential/src/app/user/user.component.ts
@@ -1,8 +1,8 @@
-import { Component, signal, computed } from '@angular/core';
+import { Component, EventEmitter, Input, Output, computed, input, output} from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
+import { type User } from './user.model';
 
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-
+// const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 
 @Component({
   selector: 'app-user',
@@ -12,18 +12,67 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css'
 })
 export class UserComponent {
+  @Input({ required: true }) user!: User;
+  // @Input({ required: true }) id!: string;
+  // @Input({ required: true }) avatar!: string;
+  // @Input({ required: true }) name!: string;
+  @Input({ required: true}) selected!: boolean;
+  
+  @Output() select = new EventEmitter<string>();
+
+  get imagePath()   {
+    return 'assets/users/' + this.user.avatar;
+  };
+
+  onSelectUser(){
+    this.select.emit(this.user.id);
+  }       
+
+  /*
+  // - using signal
+  id = input.required<string>();
+  avatar = input.required<string>();
+  name = input.required<string>();
+  select = output<string>();
+
+  imagePath = computed(() => {
+    return 'assets/users/' + this.avatar();
+  })
+
+  onSelectUser() {
+    this.select.emit(this.id);
+  }
+  */
+
+
+
+  /*
+  - traditional way
+  selectedUser = DUMMY_USERS[randomIndex];
+
+  get imagePath(){
+    return 'assets/users/' + this.selectedUser.avatar;
+  }
+
+  onSelectUser(){
+    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+    this.selectedUser = DUMMY_USERS[randomIndex];
+  }
+  */
+
+  /*
+  - using signal 
+  - add soft brackets () for each variable in html
+
   selectedUser = signal(DUMMY_USERS[randomIndex]);
 
-  //if you have signal, use this way instead get imagePath()
   imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar)
-
-  // get imagePath(){
-  //   return 'assets/users/' + this.selectedUser.avatar;
-  // }
 
   onSelectUser(){
     const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
     this.selectedUser.set(DUMMY_USERS[randomIndex]);
     // this.selectedUser = DUMMY_USERS[randomIndex];
   }
+  */
+
 }
