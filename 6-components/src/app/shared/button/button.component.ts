2a980cd117c0c6efc90238d2395ee9da157a08d6
CocoTapi <shioriavino@gmail.com>
2025-02-06 14:58:06 -0500

    remove unnecessary wrapper and add host for styling

---
+++ 6-components/src/app/shared/button/button.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  // you can name it as attribute
+  selector: 'button[appButton], a[appButton]',
+  standalone: true,
+  imports: [],
+  templateUrl: './button.component.html',
+  styleUrl: './button.component.css'
+})
+export class ButtonComponent {
+
+}
---
+++ 6-components/src/app/shared/control/control.component.ts
@@ -0,0 +1,34 @@
+import { Component, ElementRef, HostBinding, HostListener, inject, input, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-control',
+  standalone: true,
+  imports: [],
+  templateUrl: './control.component.html',
+  styleUrl: './control.component.css',
+  // disable the style scoping 
+  // styling for input and textarea inside ng-content
+  encapsulation: ViewEncapsulation.None,
+  // styling for control without wrapping with p 
+  host: {
+    class: "control",
+    '(click)': 'onClick()'
+  }
+})
+export class ControlComponent {
+  // this is discouraged.
+  // @HostBinding('class') className = 'control';
+  // @HostListener('click') onClick() {
+  //   console.log('Clicked!')
+  // }
+
+  label = input.required<string>();
+
+  // if you need programmatic access to the host element, use this. But it's rare.
+  private el = inject(ElementRef);
+
+  onClick() {
+    console.log('Clicked!')
+    console.log(this.el);
+  }
+}
