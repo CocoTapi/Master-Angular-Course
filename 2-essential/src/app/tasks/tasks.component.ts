afdb147350658bf484f4ee3b0cd1a086879e34cd
CocoTapi <shioriavino@gmail.com>
2025-02-05 22:14:42 -0500

    Add reusable card component and service. Move get, add, remove functions into service. Save data in local storage.

--- 2-essential/src/app/tasks/tasks.component.ts
+++ 2-essential/src/app/tasks/tasks.component.ts
@@ -2,32 +2,7 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { TaskComponent } from "./task/task.component";
 import { NewTaskComponent } from "./new-task/new-task.component";
 import { type NewTaskData } from './task/task.model';
-
-const dummyTasks = [
-  {
-    id: 't1',
-    userId: 'u1',
-    title: 'Master Angular',
-    summary:
-      'Learn all the basic and advanced features of Angular & how to apply them.',
-    dueDate: '2025-12-31',
-  },
-  {
-    id: 't2',
-    userId: 'u3',
-    title: 'Build first prototype',
-    summary: 'Build a first prototype of the online shop website',
-    dueDate: '2024-05-31',
-  },
-  {
-    id: 't3',
-    userId: 'u3',
-    title: 'Prepare issue template',
-    summary:
-      'Prepare and describe an issue template which will help with project management',
-    dueDate: '2024-06-15',
-  },
-]
+import { TasksService } from './tasks.service';
 
 @Component({
   selector: 'app-tasks',
@@ -37,39 +12,27 @@ const dummyTasks = [
   styleUrl: './tasks.component.css'
 })
 export class TasksComponent {
-  @Input() userId?: string;
-  @Input() name?: string;
+  @Input() userId!: string;
+  @Input() name!: string;
   // @Input() name: string | undefined;
 
   isAddingTask = false;
 
-  tasks = dummyTasks;
+  //use service - dependency injection
+  constructor(private tasksService: TasksService) {}
 
   get selectedUserTasks() {
-    return this.tasks.filter((task) => task.userId === this.userId)
-  }
-
-  onCompleteTask(id: string){
-    this.tasks = this.tasks.filter((task) => task.id !== id);
+    return this.tasksService.getUserTasks(this.userId);
   }
 
   onStartAddTask(){
     this.isAddingTask = true;
   }
 
-  onCancelAddTask(){
+  onCloseAddTask(){
     this.isAddingTask = false;
   }
 
-  onAddTask( taskData: NewTaskData){
-    this.tasks.unshift({
-      id: new Date().getTime().toString(),
-      userId: this.userId!,
-      title: taskData.title,
-      summary: taskData.summary,
-      dueDate: taskData.date
-    })
-    this.isAddingTask = false;
-  }
+
 
 }
