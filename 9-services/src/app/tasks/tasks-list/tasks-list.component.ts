d9515d9c2e975c78b0a8b13ec1d5a3d9c0f3f093
CocoTapi <shioriavino@gmail.com>
2025-02-10 12:34:19 -0500

    inject option status values

--- 9-services/src/app/tasks/tasks-list/tasks-list.component.ts
+++ 9-services/src/app/tasks/tasks-list/tasks-list.component.ts
@@ -2,6 +2,7 @@ import { Component, signal, inject, computed } from '@angular/core';
 
 import { TaskItemComponent } from './task-item/task-item.component';
 import { TasksService } from '../tasks.service';
+import { TASK_STATUS_OPTIONS, taskStatusOptionProvider } from '../task.model';
 
 @Component({
   selector: 'app-tasks-list',
@@ -9,11 +10,14 @@ import { TasksService } from '../tasks.service';
   templateUrl: './tasks-list.component.html',
   styleUrl: './tasks-list.component.css',
   imports: [TaskItemComponent],
+  providers: [taskStatusOptionProvider]
 })
 export class TasksListComponent {
   // you can use constructor to inject tasksService but this is alternative way.
   private tasksService = inject(TasksService);
   private selectedFilter = signal<string>('all');
+  taskStatusOptions = inject(TASK_STATUS_OPTIONS);
+
   // computed is a feature related to signal.
   // this will return a new signal, which depends on other signals and which will be recomputed whenever any of dependent signals change
   tasks = computed(() => {
