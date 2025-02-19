4806213f761f7c8ac0cf250182199f22647facf9
CocoTapi <shioriavino@gmail.com>
2025-02-19 11:51:56 -0500

    add nested routes and link. Load data based on route params in a child route.

--- 14-routing/src/app/users/user-tasks/user-tasks.component.ts
+++ 14-routing/src/app/users/user-tasks/user-tasks.component.ts
@@ -1,18 +1,36 @@
-import { Component, input, inject, computed } from '@angular/core';
+import { Component, input, inject, computed, OnInit, DestroyRef } from '@angular/core';
 import { UsersService } from '../users.service';
+import { ActivatedRoute, RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-user-tasks',
   standalone: true,
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './user-tasks.component.html',
   styleUrl: './user-tasks.component.css',
 })
-export class UserTasksComponent {
-  userId = input.required<string>();
+export class UserTasksComponent implements OnInit {
+  // userId = input.required<string>();
   private usersService = inject(UsersService);
+  private activateRoute = inject(ActivatedRoute);
+  private destroyRef = inject(DestroyRef);
 
-  userName = computed(() => 
-    this.usersService.users.find(u => u.id === this.userId())?.name
-  );
+  // userName = computed(() => 
+  //   this.usersService.users.find(u => u.id === this.userId())?.name
+  // );
+
+  userName = '';
+
+  ngOnInit(): void {
+    const subscription = this.activateRoute.paramMap.subscribe({
+      next: paramMap => {
+        this.userName = 
+          this.usersService.users.find((u) => u.id === paramMap.get('userId'))
+            ?.name || ''
+      }
+    });
+
+    this.destroyRef.onDestroy(() => subscription.unsubscribe());
+  }
 
 }
