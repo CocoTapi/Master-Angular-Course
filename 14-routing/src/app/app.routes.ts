0a66b7fdb136cfe706aca9a7fe38c35dff6bb8f6
CocoTapi <shioriavino@gmail.com>
2025-02-19 17:01:00 -0500

    add resolve and data properties

--- 14-routing/src/app/app.routes.ts
+++ 14-routing/src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from "@angular/router";
 import { NoTaskComponent } from "./tasks/no-task/no-task.component";
-import { UserTasksComponent } from "./users/user-tasks/user-tasks.component";
+import { resolveUserName, UserTasksComponent } from "./users/user-tasks/user-tasks.component";
 import { NotFoundComponent } from "./not-found/not-found.component";
 import { routes as userRoutes } from "./users/users.routes";
 
@@ -13,6 +13,12 @@ export const routes: Routes = [
         path: 'users/:userId', //<your-domain>/users/<user-id>
         component: UserTasksComponent,
         children: userRoutes,
+        data: {
+            message: 'Hello!'
+        },
+        resolve: {
+            userName: resolveUserName    
+        }
     }, 
     {
         // no other path matches the path
--- 14-routing/src/app/users/user-tasks/user-tasks.component.ts
+++ 14-routing/src/app/users/user-tasks/user-tasks.component.ts
@@ -1,6 +1,6 @@
 import { Component, input, inject, computed, OnInit, DestroyRef } from '@angular/core';
 import { UsersService } from '../users.service';
-import { ActivatedRoute, RouterLink, RouterOutlet } from '@angular/router';
+import { ActivatedRoute, ActivatedRouteSnapshot, ResolveFn, RouterLink, RouterOutlet, RouterStateSnapshot } from '@angular/router';
 
 @Component({
   selector: 'app-user-tasks',
@@ -9,28 +9,66 @@ import { ActivatedRoute, RouterLink, RouterOutlet } from '@angular/router';
   templateUrl: './user-tasks.component.html',
   styleUrl: './user-tasks.component.css',
 })
-export class UserTasksComponent implements OnInit {
+export class UserTasksComponent {
   // userId = input.required<string>();
-  private usersService = inject(UsersService);
-  private activateRoute = inject(ActivatedRoute);
+  message = input.required<string>()
+  userName = input.required<string>();
+  
+  // private activatedRoute = inject(ActivatedRoute);
+
+
+
+
+  // private usersService = inject(UsersService);
+  private activatedRoute = inject(ActivatedRoute);
   private destroyRef = inject(DestroyRef);
+ 
 
   // userName = computed(() => 
   //   this.usersService.users.find(u => u.id === this.userId())?.name
   // );
 
-  userName = '';
+  // userName = '';
 
-  ngOnInit(): void {
-    const subscription = this.activateRoute.paramMap.subscribe({
-      next: paramMap => {
-        this.userName = 
-          this.usersService.users.find((u) => u.id === paramMap.get('userId'))
-            ?.name || ''
-      }
-    });
+  // if you don't use resolve function at router, you can get userName as below
+  // ngOnInit(): void {
+  //   console.log("Input Data:" + this.message())
+  //   // snapshot is not observable. actual object
+  //   // snapshot won't be re-executed. never change depending on dynamic path
+  //   console.log(this.activatedRoute.snapshot);
+  //   console.log(this.activatedRoute.snapshot.paramMap.get('userId'));
 
-    this.destroyRef.onDestroy(() => subscription.unsubscribe());
-  }
+  //   const subscription = this.activatedRoute.paramMap.subscribe({
+  //     next: paramMap => {
+  //       this.userName = 
+  //         this.usersService.users.find((u) => u.id === paramMap.get('userId'))
+  //           ?.name || ''
+  //     }
+  //   });
 
+  //   this.destroyRef.onDestroy(() => subscription.unsubscribe());
+  // }
+
+  // use resolve and data at router, and get userName and message (you can do just using input() though)
+  // ngOnInit(): void {
+  //   const subscription = this.activatedRoute.data.subscribe({
+  //     next: (data) => {
+  //       console.log(data);
+  //     }
+  //   });
+
+  //   this.destroyRef.onDestroy(() => subscription.unsubscribe());
+  // }
+
+}
+
+export const resolveUserName: ResolveFn<string> = (
+  activatedRoute: ActivatedRouteSnapshot, 
+  routerState: RouterStateSnapshot
+) => {
+  const usersService = inject(UsersService);
+  const userName
+ = usersService.users.find((u) => u.id === activatedRoute.paramMap.get('userId'))
+  ?.name || '';
+ return userName;    
 }
